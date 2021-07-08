f2b09c83723b44206a63dd2100b530a365b45e17
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-08 22:16:17 +0000

    Customized admin

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Workouts, Category
+
 # Register your models here.
-admin.site.register(Workouts)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class WorkoutsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Workouts, WorkoutsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- workouts/models.py
+++ workouts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +17,10 @@ class Category(models.Model):
 
 
 class Workouts(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Workouts'
+        
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
