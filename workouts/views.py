1e8e52072cf637bb4ba35b1cf3eab07e1a5cf9f2
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-10 21:03:18 +0000

    sorted the categories in search form

--- workouts/views.py
+++ workouts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Workouts
+from django.contrib import messages
+from django.db.models import Q
+from .models import Workouts, Category
 
 # Create your views here.
 
@@ -7,9 +9,28 @@ def all_workouts(request):
     """ A view to show all products, including sorting and search queries """
 
     workouts = Workouts.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            workouts = workouts.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('workouts'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            workouts = workouts.filter(queries)
 
     context = {
         'workouts': workouts,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'workouts/workouts.html', context)
