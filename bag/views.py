be12ae57ba5f8ca41a877f6865524f89606b475e
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-16 22:39:43 +0000

    completed edit_bag in the bag views.py

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -19,3 +19,16 @@ def add_to_bag(request, workouts_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def edit_bag(request, workouts_id):
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+            bag[workouts_id] = quantity
+    else:
+            bag.pop(workouts_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
