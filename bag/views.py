f7cb12445da9c9499e473983aaf530bbdffe09ec
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-17 14:23:19 +0000

    Created toasts in includes folder

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from workouts.models import Workouts
 
 def view_bag(request):
     return render(request, 'bag/bag.html')
@@ -7,7 +9,7 @@ def view_bag(request):
 
 def add_to_bag(request, workouts_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    workouts = Workouts.objects.get(pk=workouts_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -16,7 +18,7 @@ def add_to_bag(request, workouts_id):
         bag[workouts_id] += quantity
     else:
         bag[workouts_id] = quantity
-
+        messages.success(request,f'Added {workouts.name} to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
