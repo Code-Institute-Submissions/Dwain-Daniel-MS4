d3a4adadb77bf31d537efc1f2dc4563a06afdd85
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-19 20:36:29 +0000

    created checkout app

--- bag/views.py
+++ bag/views.py
@@ -19,19 +19,25 @@ def add_to_bag(request, workouts_id):
         bag[workouts_id] += quantity
     else:
         bag[workouts_id] = quantity
-        messages.success(request,f'Added {workouts.name} to your bag')
+        messages.success(request,f'Added {workouts.name} workout plan to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def edit_bag(request, workouts_id):
+    workouts = Workouts.objects.get(pk=workouts_id)
+
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
             bag[workouts_id] = quantity
+            messages.success(request,f'Updated {workouts.name} quantity to your bag')
+
     else:
             bag.pop(workouts_id)
+            messages.success(request,f'Removed {workouts.name} from your bag')
+
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -39,10 +45,14 @@ def edit_bag(request, workouts_id):
  
 def remove_from_bag(request, workouts_id):
     """remove the specified product from the bag"""
+    workouts = Workouts.objects.get(pk=workouts_id)
+
     try:
         bag = request.session.get('bag', {})
 
         bag.pop(workouts_id)
+        messages.success(request,f'Removed {workouts.name} from your bag')
+
 
 
         request.session['bag'] = bag
@@ -51,4 +61,5 @@ def remove_from_bag(request, workouts_id):
     
 
     except Exception as e:
+        messages.error(request,f'Error removing {workouts.name} from your bag')
         return HttpResponse(status=500)
