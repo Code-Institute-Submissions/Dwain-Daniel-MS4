e86ecf7822f24ca633d0896c7708c27ebe98a69a
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-07-30 23:16:48 +0000

    heroku deployment part 2

--- power_gym/settings.py
+++ power_gym/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dwain-power-gym.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,12 +116,18 @@ WSGI_APPLICATION = 'power_gym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
